44da542213fef0dd8512f34cbf65665ef47bc4f7
Tharushi Chethana <ththarushi3@gmail.com>
2022-07-10 16:53:58 +0530

    Tharu

---
+++ HelloWorld.scala
@@ -0,0 +1,9 @@
+object Q2 extends App{
+  var (k,i,j,m,n,f,g,c)=(2,2,2,5,5,12.0,4.0,"x")
+  printf("%d",k+12*m)
+  printf("\n%d",m/j)
+  printf("\n%d",n%j)
+  printf("\n%d",m/j*j)
+  printf("\n%f",f+10*5+g)
+  //printf("\n%d",++i*n)  scala not support increment opertor
+}
